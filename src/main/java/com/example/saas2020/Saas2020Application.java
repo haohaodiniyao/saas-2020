88dc40222f66b1555ffede81ae0d241d88d4a201
yaokai <yaokai@01zhuanche.com>
2020-02-25 20:27:50 +0800

    Initial commit

---
+++ src/main/java/com/example/saas2020/Saas2020Application.java
@@ -0,0 +1,13 @@
+package com.example.saas2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saas2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Saas2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/saas2020/Saas2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saas2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Saas2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
